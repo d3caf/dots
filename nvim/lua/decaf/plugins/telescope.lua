67f31d3e024103a5d2d7690f63ac8b41d57d33b9
Andrew Anguiano <aanguiano@newrelic.com>
2024-08-06 12:21:46 -0400

    Add more telescope commands

--- nvim/lua/decaf/plugins/telescope.lua
+++ nvim/lua/decaf/plugins/telescope.lua
@@ -6,12 +6,14 @@ return {
     keys = function()
       local tele = require('telescope.builtin')
       return {
-        { '<leader>ff',      tele.find_files, 'n', { desc = "Find files" } },
-        { '<leader><Space>', tele.find_files, 'n', { desc = "Find files" } },
-        { '<leader>fg',      tele.live_grep,  'n', { desc = "Find by grep" } },
-        { '<leader>/',       tele.live_grep,  'n', { desc = "Find by grep" } },
-        { '<leader>fb',      tele.buffers,    'n', { desc = "Find buffers" } },
-        { '<leader>fh',      tele.help_tags,  'n', { desc = "Find help" } },
+        { '<leader>ff',      tele.find_files,            'n', { desc = "Find files" } },
+        { '<leader><Space>', tele.find_files,            'n', { desc = "Find files" } },
+        { '<leader>fg',      tele.live_grep,             'n', { desc = "Find by grep" } },
+        { '<leader>/',       tele.live_grep,             'n', { desc = "Find by grep" } },
+        { '<leader>fb',      tele.buffers,               'n', { desc = "Find buffers" } },
+        { '<leader>fh',      tele.help_tags,             'n', { desc = "Find help" } },
+        { '<leader>fs',      tele.lsp_document_symbols,  'n', { desc = "Find symbols in document" } },
+        { '<leader>fS',      tele.lsp_workspace_symbols, 'n', { desc = "Find symbols in workspace" } }
       }
     end,
   },
