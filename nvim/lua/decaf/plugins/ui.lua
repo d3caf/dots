69d6c4234de1ef5b2a4908fef3d48c5457189119
Andrew Anguiano <aanguiano@newrelic.com>
2024-08-14 15:45:26 -0400

    Add indentscope and barbecue

--- nvim/lua/decaf/plugins/ui.lua
+++ nvim/lua/decaf/plugins/ui.lua
@@ -6,8 +6,33 @@ return {
       "nvim-tree/nvim-web-devicons"
     },
     init = function() vim.g.barbar_auto_setup = false end,
-    config = {
+    opts = {
       auto_hide = 2
     }
+  },
+  -- Indent guides
+  {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    opts = function()
+      return {
+        draw = {
+          animation = require('mini.indentscope').gen_animation.none(),
+        },
+        symbol = '│'
+      }
+    end
+  },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
   }
 }
